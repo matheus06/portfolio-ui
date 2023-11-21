bca5770f9bf1fd4454ce724f445107801a89eecf
Matheus Sexto <matheus.sexto@gmail.com>
2023-11-21 21:04:35 +0100

    update

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -71,7 +71,7 @@ resource "azurerm_linux_web_app" "webappapi" {
   name                  = var.api_app_name
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.functionappserviceplan.id
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
   https_only            = true
   
   site_config { 
@@ -116,7 +116,7 @@ resource "azurerm_linux_function_app" "functionapi" {
 
   storage_account_name       = azurerm_storage_account.storageacc.name
   storage_account_access_key = azurerm_storage_account.storageacc.primary_access_key
-  service_plan_id            = azurerm_service_plan.appserviceplan.id
+  service_plan_id            = azurerm_service_plan.functionappserviceplan.id
 
   site_config {}
 }
