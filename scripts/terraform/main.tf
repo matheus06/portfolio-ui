5c47e67688a17a1f27dbd64232dc311d8b1fce50
Matheus Sexto <matheus.sexto@gmail.com>
2023-11-21 20:38:22 +0100

    update name storage

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -80,7 +80,7 @@ resource "azurerm_linux_web_app" "webappapi" {
 
 # Create the storage acc
 resource "azurerm_storage_account" "storageacc" {
-  name                     = "portfoliostorageacc"
+  name                     = "matheusportfoliostorageacc"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
