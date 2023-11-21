6ee17e4d118cf3da64187a8ad9633181af425d87
Matheus Sexto <matheus.sexto@gmail.com>
2023-11-21 22:18:14 +0100

    test

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the Function Linux App Service Plan
 resource "azurerm_service_plan" "functionappserviceplan" {
   name                = var.function_service_plan_name
-  location            = var.location
+  location            = "East US"
   resource_group_name = azurerm_resource_group.sharedrg.name
   os_type             = "Linux"
   sku_name            = "Y1"
